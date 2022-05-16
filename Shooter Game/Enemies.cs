b69ac965ba11b1c8933593bb63eda7d8d40eb934
LizaFedorova <103308669+LizaFedorova@users.noreply.github.com>
2022-05-16 15:37:16 +0500

    Add files via upload

---
+++ Shooter Game/Enemies.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shooter_Game
+{
+    class Enemies
+    {
+        int speed;
+        Image image;
+        Rectangle rect;
+        int index;
+        int healthpoint;
+        public Enemies()
+        {
+            healthpoint = 3;
+            //image = Properties.Resources.heli1;
+            rect = new Rectangle(0, 50, 100, 54);
+            speed = 10;
+            index = 0;
+        }
+
+    }
+}
