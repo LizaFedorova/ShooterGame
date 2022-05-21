6508e869982615abe2f605fb10765c213fe6b80e
LizaFedorova <103308669+LizaFedorova@users.noreply.github.com>
2022-05-21 13:55:44 +0500

    Update Player.cs

--- Shooter Game/Player.cs
+++ Shooter Game/Player.cs
@@ -1,34 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
 namespace Shooter_Game
 {
     public class Player
     {
-        Point location = new Point(300, 50);
-
-
-
-        /*public void New_player(ShooterGame F)
+        public Point Location;
+        public int speed;
+        public Player()
         {
-            size = F.imageP.Size;
-            point.X = 0;
-            point.Y = 0;
-            Rectangle rec = new Rectangle(point, size);
+            Location.X = 300;
+            Location.Y = 50;
+            speed = 5;
         }
-        public void Show_player(ShooterGame F, int x, int y)
-        {
-            F.g.ResetClip();
-            point.X = x - size.Width / 2;
-            point.Y = y;
-            Rectangle rec = new Rectangle(point, size);
-            F.g.DrawImage(F.imageP, point);
-            //F.g.ExcludeClip(reg);
-        }*/
     }
 }
