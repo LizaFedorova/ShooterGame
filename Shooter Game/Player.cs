b29306189dad2c871cf286e16eb601c8ca45102a
LizaFedorova <103308669+LizaFedorova@users.noreply.github.com>
2022-05-26 17:09:54 +0500

    Update Player.cs

--- Shooter Game/Player.cs
+++ Shooter Game/Player.cs
@@ -1,15 +1,31 @@
 using System.Drawing;
+using System.Windows.Forms;
+
 namespace Shooter_Game
 {
     public class Player
     {
         public Point Location;
-        public int speed;
+        public int Speed;
+        public Rectangle Rect;
+        public int width;
+        public int height;
+        public Image Image;
         public Player()
         {
-            Location.X = 300;
-            Location.Y = 50;
-            speed = 5;
+            Location.X = 50;
+            Location.Y = 300;
+            Speed = 5;
+            width = 80;
+            height = 130;
+            Image = Properties.Resources.player_stands;
+            Rect = new Rectangle(Location.X, Location.Y, width, height);
+        }
+
+        public Rectangle Rectangle()
+        {
+            return Rect;
         }
     }
+
 }
