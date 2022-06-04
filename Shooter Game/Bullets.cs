187bf08540c9bd7dc1ee9329404ff0967ebea49b
LizaFedorova <103308669+LizaFedorova@users.noreply.github.com>
2022-06-04 12:02:39 +0500

    Update Bullets.cs

--- Shooter Game/Bullets.cs
+++ Shooter Game/Bullets.cs
@@ -1,19 +1,32 @@
-﻿using System.Drawing;
+using System.Drawing;
 using System.Windows.Forms;
 namespace Shooter_Game
 {
     class Bullets
     {
         public int Speed;
-        public int Width;
-        public int Height;
+        public int width;
+        public int height;
         public int x;
         public int y;
+        PictureBox bullets;
         public Bullets()
         {
             Speed = 80;
-            Height = 5;
-            Width = 20;
+            height = 5;
+            width = 20;
+        }
+
+        public void NewBullets(Form form, int x, int y)
+        {
+            bullets = new PictureBox();
+            bullets.BackColor = Color.White;
+            bullets.BorderStyle = BorderStyle.None;
+            bullets.Height = height;
+            bullets.Width = width;
+            bullets.Location = new Point(x, y);
+            bullets.Name = "Bullet";
+            form.Controls.Add(bullets);
         }
     }
 }
