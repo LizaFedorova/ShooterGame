1ce8650a58ddf41ff22b32c8e1d65a4941e7416c
LizaFedorova <103308669+LizaFedorova@users.noreply.github.com>
2022-06-04 12:03:01 +0500

    Add files via upload

---
+++ Shooter Game/Enemies.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Shooter_Game
+{
+    class Enemies
+    {
+        public int Size;
+        public int Speed;
+        public Image Image;
+        Random random;
+        public Enemies()
+        {
+            random = new Random();
+            Size = random.Next(60, 80);
+            Speed = 3;
+            Image = Properties.Resources.enemy2;
+        }
+    }
+}
